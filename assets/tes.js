bd5006c973fce538ca92af9ce5c121732d0a8b82
Teguh Saint <barayacoffeejombang@gmail.com>
2024-01-15 02:09:07 +0700

    ei

--- assets/tes.js
+++ assets/tes.js
@@ -1,5 +1,5 @@
 var data = JSON.parse(localStorage.getItem("pendaftaranData"));
-
+var kode_regis = randomInteger(99999, 99999999);
 var isijawaban = 0;
 $("input[type='radio']").click(function () {
   isijawaban++;
@@ -13,6 +13,7 @@ document.getElementById("namaPengguna").textContent = pendaftaranData.username;
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function kirim() {
   if (pendaftaranData) {
     var soalContainer = document.getElementById("soalContainer");
@@ -40,38 +41,70 @@ function kirim() {
       jawabanBenar++;
     }
 
-  
     // Menentukan apakah pengguna lulus atau tidak
     var lulus = jawabanBenar >= 4;
-    var pesanHasil = lulus ? "Selamat, Anda lulus! Silahkan melakukan pembayaran pada menu pembayaran!" : "Maaf, Anda tidak lulus.";
+    var pesanHasil = lulus ? "" : "Maaf, Anda tidak lulus.";
+    $("#modalId").modal("show");
+    $("#kodereg").text(kode_regis);
+
+    $("#dataregis").append(`
+    <table class="table table-bordered">
+    <tbody>
+      <tr>
+        <td>Nama Lengkap</td>
+        <td>${data.nama}</td>
+      </tr>
+      <tr>
+        <td>Tanggal Lahir</td>
+        <td>${data.tanggalLahir}</td>
+      </tr>
+      <tr>
+        <td>Alamat</td>
+        <td>${data.alamat}</td>
+      </tr>
+      <tr>
+        <td>Kelas Yang Di Pilih</td>
+        <td>${data.kelas}</td>
+      </tr>
+    </tbody>
+  </table>
+    
+    `);
+
     if (jawabanBenar > 4) {
       $.ajax({
         type: "POST",
         url: "https://informatikaunwaha.com/tugasapi/naufal/siswa.php",
         data: {
-          nisns: randomInteger(900,99999),
+          nisns: randomInteger(900, 99999),
+          koderegister: kode_regis,
           nama: data.nama,
           kelas: data.kelas,
           alamat: data.alamat,
           tglLahir: data.tanggalLahir,
+          status: 0,
         },
         dataType: "JSON",
         success: function (response) {
-          alert(pesanHasil);
+          //     alert(pesanHasil);
           console.log(response);
-          localStorage.clear()
+          localStorage.clear();
           JSON.parse(localStorage.getItem("pendaftaranData"));
           document.getElementById("namaPengguna").textContent = pendaftaranData.username;
-          
         },
       });
     } else {
-      alert(pesanHasil);
-      localStorage.setItem("halaman", "form.html");
-      halamanini = localStorage.getItem("halaman");
-      window.location = "index.html";
+      // alert(pesanHasil);
+      // localStorage.setItem("halaman", "form.html");
+      // halamanini = localStorage.getItem("halaman");
+      // window.location = "index.html";
     }
   } else {
     alert("Data pendaftaran tidak ditemukan.");
   }
+}
+
+function direk() {
+  localStorage.setItem("halamanakhir", '{"halaman":"pembayaran/pembayaran.html","id":"#registrasi"}');
+  window.location.reload();
 }
